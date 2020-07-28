7bdf72edd1ddd9aac1115b9553674a6f12bace01
soumith <soumithganji@gmail.com>
2020-07-28 16:51:29 +0530

    'init'

---
+++ precache-manifest.690ccc01a83d2b376b41bec230a65c9f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93585898b2727c7805fde101e4c217a5",
+    "url": "/confusion/index.html"
+  },
+  {
+    "revision": "82251408074fc1be9f78",
+    "url": "/confusion/static/css/2.fd308fc8.chunk.css"
+  },
+  {
+    "revision": "8eedb416f255dee44b38",
+    "url": "/confusion/static/css/main.c87f294e.chunk.css"
+  },
+  {
+    "revision": "82251408074fc1be9f78",
+    "url": "/confusion/static/js/2.2410d294.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/confusion/static/js/2.2410d294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eedb416f255dee44b38",
+    "url": "/confusion/static/js/main.74c490d4.chunk.js"
+  },
+  {
+    "revision": "93e338425105545936e2",
+    "url": "/confusion/static/js/runtime-main.dfde7455.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
